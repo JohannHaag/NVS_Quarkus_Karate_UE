bccb3247ea5afba0d9ef0a468398910179f33fba
JohannHaag <31926853+JohannHaag@users.noreply.github.com>
2020-12-07 06:42:02 +0100

    Update PersonDao.java

--- quarkus-karate-demo/src/main/java/at/htl/karate/control/PersonDao.java
+++ quarkus-karate-demo/src/main/java/at/htl/karate/control/PersonDao.java
@@ -8,23 +8,4 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class PersonDao implements PanacheRepository<Person> {
 
-    public Person update(Person updated) {
-        Person persistedPerson = findById(updated.id);
-        if (persistedPerson != null) {
-            persistedPerson.id = updated.id;
-            persistedPerson.birth = updated.birth;
-            persistedPerson.name = updated.name;
-            persistedPerson.status = updated.status;
-        }
-        return persistedPerson;
-    }
-
-
-    public Person deletePerson(Person deletedperson){
-        Person persistedPerson= findById(deletedperson.id);
-        if(persistedPerson!= null) {
-            this.delete("id = ?1", deletedperson.id);
-        }
-        return persistedPerson;
-    }
 }
